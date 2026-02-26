5602e0e9d5aa10e5ec4bffd5c606aed05775113b
mvsc-code-maker <mvsc.com@gmail.com>
2026-02-26 19:57:26 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/DD-Motors',
-  assetPrefix: '/DD-Motors/',
+  // Remova o basePath e o assetPrefix para funcionar na raiz da Vercel
   images: {
     unoptimized: true,
   },
-  // As duas linhas abaixo forçam o site a ir pro ar mesmo com pequenos erros de IA
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
 };
 
 export default nextConfig;
